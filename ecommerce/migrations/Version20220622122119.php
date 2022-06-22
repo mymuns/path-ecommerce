59165f2bd453a5f450ed1b07c924686ae20f4ce6
Erman Titiz <erman.titiz@kabipartners.com>
2022-06-22 16:06:17 +0300

    fixed shippingdate can not be null rule

---
+++ ecommerce/migrations/Version20220622122119.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220622122119 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE customer_order CHANGE shipping_date shipping_date DATETIME DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE customer_order CHANGE shipping_date shipping_date DATETIME NOT NULL');
+    }
+}
