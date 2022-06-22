d1c5607e1b7cdaf67feb5820f0906959f614bfa1
Erman Titiz <erman.titiz@kabipartners.com>
2022-06-22 16:09:04 +0300

    fixed json response

--- ecommerce/src/Controller/BaseController.php
+++ ecommerce/src/Controller/BaseController.php
@@ -2,14 +2,26 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\Persistence\ObjectManager;
+;
 
 class BaseController extends AbstractController
 {
+    private ObjectManager $_em;
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->_em = $em;
+    }
 
-    public function response($data, $message=null,$status=200): JsonResponse
+    public function getRepository($persistenObject)
+    {
+        return $this->_em->getRepository($persistenObject);
+    }
+    public function response($data, $message=null, $status=200): JsonResponse
     {
         return $this->json([
             'message' => $message ?? 'Başarılı',
