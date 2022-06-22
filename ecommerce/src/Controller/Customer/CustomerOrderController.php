b0ce6e1d204bd12ee3d2b5d56b8cc8e0d0970cc3
Erman Titiz <erman.titiz@kabipartners.com>
2022-06-22 16:21:44 +0300

    added customer order update function

--- ecommerce/src/Controller/Customer/CustomerOrderController.php
+++ ecommerce/src/Controller/Customer/CustomerOrderController.php
@@ -27,7 +27,7 @@ class CustomerOrderController extends BaseController
         return $this->response($orders);
     }
 
-    #[Route('/customer/show/{{orderCode}}', name: 'app_customer_order_show')]
+    #[Route('/customer/show/{orderCode}', name: 'app_customer_order_show')]
     public function show($orderCode): JsonResponse
     {
         $order = $this->getRepository(CustomerOrder::class)->showOrderByOrderCode($this->getUser(),$orderCode);
@@ -41,9 +41,36 @@ class CustomerOrderController extends BaseController
         }
         return $this->response($order, $message, $status);
     }
+
     #[Route('/customer/create', name: 'app_customer_order_show')]
     public function create(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager): JsonResponse
     {
+        return $this->createUpdate($request,$validator,$entityManager);
+    }
+
+    #[Route('/customer/update/{id}', name: 'app_customer_order_update')]
+    public function update(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager, $id): JsonResponse
+    {
+        $customerOrder = $this->getRepository(CustomerOrder::class)->find($id);
+        if(!$customerOrder)
+        {
+            return $this->response(null, "Sipariş bulunamadı");
+        }
+        return $this->createUpdate($request,$validator,$entityManager,$customerOrder);
+    }
+
+    private function createUpdate(Request $request, ValidatorInterface $validator, EntityManagerInterface $entityManager, ?CustomerOrder $customerOrder): JsonResponse
+    {
+        if(!$customerOrder)
+        {
+            $customerOrder = new CustomerOrder();
+        }
+
+        if(!is_null($customerOrder->getShippingDate()))
+        {
+            return $this->response(null, "Bu siparişi güncelleyemezsiniz",500);
+        }
+
         $parametersAsArray = [];
         if ($content = $request->getContent()) {
             $parametersAsArray = json_decode($content, true);
@@ -81,13 +108,12 @@ class CustomerOrderController extends BaseController
             return $this->response(null,"Bu ürün mevcut değil",404);
         }
 
-        $customerOrder = new CustomerOrder();
         $customerOrder->setAddress($parametersAsArray['address']);
         $customerOrder->setQuantity($parametersAsArray['quantity']);
         $customerOrder->setProduct($product);
         $customerOrder->setUser($this->getUser());
         $customerOrder->setCreatedOrderCode();
-
+        // $customerOrder->setShippingDate(new \DateTime('now'));
         $entityManager->persist($customerOrder);
         $entityManager->flush();
 
