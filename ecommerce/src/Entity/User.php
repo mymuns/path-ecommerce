4d407f09b457a18d9035db708cb7db7a5bd6c696
Erman Titiz <erman.titiz@kabipartners.com>
2022-06-22 12:25:04 +0300

    added customerOrder field and related to customerOrder entity

--- ecommerce/src/Entity/User.php
+++ ecommerce/src/Entity/User.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -23,6 +25,14 @@ class User implements UserInterface
 
     #[ORM\Column(type: 'string', length: 255)]
     private $password;
+
+    #[ORM\OneToMany(mappedBy: 'user', targetEntity: CustomerOrder::class)]
+    private $customerOrders;
+
+    public function __construct()
+    {
+        $this->customerOrders = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -93,6 +103,36 @@ class User implements UserInterface
     public function eraseCredentials()
     {
         // TODO: Implement eraseCredentials() method.
+    }
+
+    /**
+     * @return Collection<int, CustomerOrder>
+     */
+    public function getCustomerOrders(): Collection
+    {
+        return $this->customerOrders;
+    }
+
+    public function addCustomerOrder(CustomerOrder $customerOrder): self
+    {
+        if (!$this->customerOrders->contains($customerOrder)) {
+            $this->customerOrders[] = $customerOrder;
+            $customerOrder->setUser($this);
+        }
+
+        return $this;
+    }
+
+    public function removeCustomerOrder(CustomerOrder $customerOrder): self
+    {
+        if ($this->customerOrders->removeElement($customerOrder)) {
+            // set the owning side to null (unless already changed)
+            if ($customerOrder->getUser() === $this) {
+                $customerOrder->setUser(null);
+            }
+        }
+
+        return $this;
     }
 
 
